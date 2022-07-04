b8560f8f3d0b33ae98b6fbc236bd52074f781a01
hmthang96 <hmthang96@gmail.com>
2022-07-05 00:37:15 +0700

    Xay dung MVC - tier 1 Presentation

--- src/main/java/com/hmthang96/qlsv/controller/admin/HomeController.java
+++ src/main/java/com/hmthang96/qlsv/controller/admin/HomeController.java
@@ -1,4 +1,23 @@
 package com.hmthang96.qlsv.controller.admin;
 
-public class HomeController {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/admin-home"})
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/views/admin/home.jsp");
+        rd.forward(req,resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
 }
--- src/main/java/com/hmthang96/qlsv/controller/student/HomeController.java
+++ src/main/java/com/hmthang96/qlsv/controller/student/HomeController.java
@@ -1,4 +1,22 @@
 package com.hmthang96.qlsv.controller.student;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class HomeController {
+@WebServlet(urlPatterns ={"/student-home"})
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/views/student/home.jsp");
+        rd.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
